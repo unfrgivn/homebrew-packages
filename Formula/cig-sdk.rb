d8b825a50756cad66b6cdd1e170de7cf61b21257
unfrgivn <bradley.ash@gmail.com>
2021-06-11 11:21:56 -0500

    feat: Add additional install commands

--- Formula/cig-sdk.rb
+++ Formula/cig-sdk.rb
@@ -3,7 +3,7 @@ class CigSdk < Formula
   homepage "https://github.com/itcig/cig-sdk"  
   url "https://github.com/itcig/cig-sdk/archive/v0.1.2.tar.gz"
   sha256 "ed65f6a65911db4db66199f78e87a7cd6f80fe25174354e4855e900b9b6f9e46"
-  head "https://github.com/itcig/cig-sdk.git"
+  head "https://github.com/itcig/cig-sdk.git", branch: "main"
 
   depends_on "ansible"
   depends_on "docker"
@@ -14,9 +14,11 @@ class CigSdk < Formula
     inreplace "bin/cig-sdk",
               "CIG_SDK_DIR='/usr/local/cig-sdk'",
               "CIG_SDK_DIR='#{prefix}'"
-
-    # system "./bin/brew-install" 
+    
     bin.install "bin/cig-sdk" => "cig"
+    libexec.install Dir["*"]
+
+    system "./bin/brew-install" 
   end
 
   # Homebrew requires tests.
