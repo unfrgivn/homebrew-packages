243768899cc06d99a5a86941ac067bfeebff68eb
unfrgivn <bradley.ash@gmail.com>
2021-11-11 20:01:47 -0600

    feat: move custom work to installed scripts

--- Formula/cig-sdk.rb
+++ Formula/cig-sdk.rb
@@ -5,25 +5,16 @@ class CigSdk < Formula
   sha256 "580f698c525a766b4ba5c561b52ad10040559b240124ff2de0a4cd5e6157a593"
   head "https://github.com/itcig/cig-sdk.git", branch: "main"
 
-  depends_on "ansible"
+  depends_on "ansible@2.9"
   depends_on "docker"
   depends_on "python@3.9"
 
   def install    
-    # Set correct working directory
-    inreplace "bin/cig-sdk",
-              "CIG_SDK_DIR='/usr/local/cig-sdk'",
-              "CIG_SDK_DIR='#{prefix}'"
-    
-    inreplace "bin/brew-install",
-              "CIG_SDK_DIR='/usr/local/cig-sdk'",
-              "CIG_SDK_DIR='#{prefix}'"
-    
     prefix.install "ansible"
     prefix.install "docker"
     bin.install "bin/cig-sdk" => "cig"
 
-    system "bash bin/brew-install"
+    system "/usr/local/bin/cig", "self", "update"
   end
 
   # Homebrew requires tests.
