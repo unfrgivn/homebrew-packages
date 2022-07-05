a3edd1c004a6788a8cf060659aefbe87566ae07c
Brad <bradley.ash@gmail.com>
2022-07-05 10:49:25 -0500

    chore: bump version to 0.10.1

--- Formula/cig-sdk.rb
+++ Formula/cig-sdk.rb
@@ -1,8 +1,8 @@
 class CigSdk < Formula
   desc "CLI to deploy CIG Dev Tools"
   homepage "https://github.com/itcig/cig-sdk"  
-  url "https://github.com/itcig/cig-sdk/archive/v0.10.0.tar.gz"
-  sha256 "c93a910a706a45c0b306f95daccca6f6c332ddc4b6a068af008da6803ef52427"
+  url "https://github.com/itcig/cig-sdk/archive/v0.10.1.tar.gz"
+  sha256 "08e2a9f617ff6bf543855d5c13ce617c79a10a4e1ea3e0419590e19bf04e49d0"
   head "https://github.com/itcig/cig-sdk.git", branch: "main"
 
   depends_on "ansible@2.9"
@@ -20,6 +20,6 @@ class CigSdk < Formula
 
   # Homebrew requires tests.
   test do
-    assert_match "CIG SDK 0.10.0", shell_output("#{bin}/cig version", 2)
+    assert_match "CIG SDK 0.10.1", shell_output("#{bin}/cig version", 2)
   end
 end
