76b32419f88c81675db0cb6f21f55bd18835320f
unfrgivn <1675472+unfrgivn@users.noreply.github.com>
2024-07-07 14:53:04 +0000

    "Brew formula update for cli-tool-test version v0.2.4"

--- formula/cli-tool-test.rb
+++ formula/cli-tool-test.rb
@@ -1,3 +1,4 @@
+# typed: true
 # frozen_string_literal: true
 
 require_relative "../lib/custom_download_strategy"
@@ -6,13 +7,13 @@ require_relative "../lib/custom_download_strategy"
 class CliToolTest < Formula
   desc "Testing a generic cli tool that can be installed with homebrew"
   homepage "https://github.com/unfrgivn/cli-tool-test"
-  url "https://github.com/unfrgivn/cli-tool-test/archive/refs/tags/v0.2.3.tar.gz"
-  sha256 "7b19dbb49f65046d9d99e6eb4e93c2826be3d740780bd5eded7b63105e3ab5e7"
+  url "https://github.com/unfrgivn/cli-tool-test/archive/refs/tags/v0.2.4.tar.gz"
+  sha256 "61f4bfce5c83b60ce6aef5c1a452e0fb4b497a7ef6e3e99818d4430512757dba"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/unfrgivn/cli-tool-test/releases/download/v0.2.3/cli-tool-test-0.2.3-darwin-arm64.tar.gz"
+      url "https://github.com/unfrgivn/cli-tool-test/releases/download/v0.2.4/cli-tool-test-0.2.4-darwin-arm64.tar.gz"
       sha256 "c07b65a87ec313bafa621bd2229e0e4d112bcb81a86620643dad4c9eb0da8e5d"
     end
   end
